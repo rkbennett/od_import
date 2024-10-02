9175627816723e07a0f26642d3f3c75fcd9231c4
rkbennett <r.k.bennett@hotmail.com>
2024-10-02 17:50:48 -0500

    adds sdist versioned folder handling to tar archive provider

--- od_import/archive_providers/tar.py
+++ od_import/archive_providers/tar.py
@@ -4,7 +4,7 @@ import logging
 import tarfile
 
 class tar(object):
-    
+
     def __init__(self, data: bytes, url_base: str, path_cache: list=[], config: object=None):
         """
         Description:
@@ -18,7 +18,15 @@ class tar(object):
         self.url = url_base
         tar_io = io.BytesIO(data)
         self.tar_bytes = tarfile.open(fileobj=tar_io, mode='r:*')
+        sdists = [item.name.split("/")[0] for item in self.tar_bytes.getmembers() if not item.isdir() and item.name.split("/")[1] == "PKG-INFO"]
         self.path_cache = path_cache + [item.name + ("/" if item.isdir() else "") for item in self.tar_bytes.getmembers()]
+        if sdists:
+            self.sdist_path_shim = {"-".join(sdist.split("-")[:-1]): sdist for sdist in sdists if "-".join(sdist.split("-")[:-1]) not in self.path_cache}
+            for package, sdist in self.sdist_path_shim.items():
+                if package not in self.path_cache:
+                    for cached_path in self.path_cache:
+                        if cached_path.startswith(f"{sdist}/") and cached_path != f"{sdist}/":
+                            self.path_cache.append(cached_path.replace(f"{sdist}/", ""))
 
     def extractor(self, url: str, path: str="", path_cache: list=[], cache_update: bool=False, config: object=None) -> bytes:
         """
@@ -35,4 +43,7 @@ class tar(object):
         """
         if path in self.path_cache:
             return b""
-        return self.tar_bytes.extractfile(url.replace(self.url, "").lstrip("/")).read() 
+        request_file = url.replace(self.url, "").lstrip("/")
+        if request_file.split("/")[0] in self.sdist_path_shim:
+            request_file = f"{self.sdist_path_shim[request_file.split('/')[0]]}/{request_file}"
+        return self.tar_bytes.extractfile(request_file).read()
